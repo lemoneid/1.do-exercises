5b619116b4d8f4ee296eafbac5bc702d15f3e8d0
worst <1931248856@qq.com>
2020-05-03 20:49:53 +0800

    OJ

--- Algorithm/1.欧拉筛.cpp
+++ Algorithm/1.欧拉筛.cpp
@@ -2,37 +2,39 @@
 	> File Name: test.cpp
 	> Author: weier 
 	> Mail: 1931248856@qq.com
-	> Created Time: 2020年05月03日 星期日 20时33分12秒
+	> Created Time: 2020年04月29日 星期三 21时52分37秒
  ************************************************************************/
 
 #include<iostream>
+#include<cstdio>
+#include<string>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+#include <stdlib.h>
 using namespace std;
 
-const int SIZE = 10000005;
-
+const int SIZE = 1005;
 int prime[SIZE] = {0}, check[SIZE] = {0};
 int p_num = 0;
-
-void init() {
-    for (int i = 2; i < SIZE; i++){
+void init(){
+    for (int i = 2; i < SIZE; i++) {
         check[i] || (prime[p_num++] = i);
-        for(int j = 0; j < p_num && i * prime[j] < SIZE; j++){
+        for (int j = 0; j < p_num && i * prime[j] < SIZE; j++) {
             check[i * prime[j]] = 1;
-            if(!(i % prime[j]))
+            if (!(i % prime[j])) {
                 break;
+            }
         }
     }
     return ;
 }
 
 int main() {
-    int a, b, i = 0;
-    cin >> a >> b;
     init();
-    while(prime[i] < a) 
-        i++;
-    while(prime[i] <= b && prime[i]){
-        cout << prime[i++] << endl;
+    for(int i = 0; i < 20; i++){
+        !i || cout << " ";
+        cout << prime[i];
     }
     return 0;
 }

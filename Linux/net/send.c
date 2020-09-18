99c5174747cb0d14f0cb5ad23db02386c3265da3
worst <1931248856@qq.com>
2020-09-18 20:05:25 +0800

    temp

--- Linux/net/send.c
+++ Linux/net/send.c
@@ -15,23 +15,29 @@ struct Msg {
 
 void send_file(const char *filename,FILE *fp,int sockfd) {
     struct Msg msg;
+    int nread;
     memset(&msg, 0, sizeof(msg));
     strcpy(msg.filename, filename);
-    while (fgets(msg.buff, sizeof(msg.buff), fp) != NULL) {
+    while ((nread = fread(msg.buff, 1, sizeof(msg.buff), fp)) > 0) {
         send(sockfd, &msg, sizeof(msg), 0);
+        printf("%s", msg.buff);
         memset(msg.buff, 0, sizeof(msg.buff));
     }
+    recv(sockfd, &msg, sizeof(msg), 0);
+    return 0;
 }
 
 void recv_file(int sockfd) {
-    while (1) {
         ssize_t nrecv;
         struct Msg msg;
-        if ((nrecv = recv(sockfd, &msg, sizeof(msg), 0)) <= 0) {
-            close(sockfd);
-            return NULL;
+        int first = 1;
+        FILE *fp = NULL;
+        while ((nrecv = recv(sockfd, &msg, sizeof(msg), 0)) > 0) {
+            if (first) {
+                fp = fopen(msg.filename, "wb");
+            }
+            fwrite(msg.buff, nrecv, 1, fp);
         }
-    }
 }
 
 
@@ -53,13 +59,11 @@ int main(int argc, char **argv) {
     char *path = "./recv.c";
     char *filename = "send";
 
-    if ((fp = fopen(path, "ab")) == NULL) {
+    if ((fp = fopen(path, "r")) == NULL) {
         printf("fopen error\n");
         perror("fopen()");
         exit(1);
     }
     send_file(filename, fp, sockfd);
-    close(sockfd);
-    fclose(fp);
     return 0;
 }

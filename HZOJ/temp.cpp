bd65dd732ff4b96203af4f43fba7d57b8cc14a0a
weier <1931248856@qq.com>
2020-09-23 21:36:43 +0800

    IPC

--- HZOJ/temp.cpp
+++ HZOJ/temp.cpp
@@ -7,3 +7,20 @@
 
 #include <iostream>
 using namespace std;
+#define MAX_N 200
+
+int pri[MAX_N], arr[MAX_N];
+int main() {
+    int n, m, l  = 0, r = 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        r += arr[i];
+    }
+    
+    while (l < r) {
+        int mid = (l + r) >> 1; 
+    }
+
+    return 0;
+}

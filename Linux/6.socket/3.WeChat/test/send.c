ddfcd8079d96691cb356041bce4240445eff11d2
weier <1931248856@qq.com>
2020-09-24 23:02:56 +0800

    ipc

---
+++ Linux/6.socket/3.WeChat/test/send.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: send.c
+	> Author: weier 
+	> Mail: 1931248856@qq.com 
+	> Created Time: Thu 24 Sep 2020 08:12:40 PM CST
+ ************************************************************************/
+
+#include "head.h"
+
+#define handle_error(msg) \
+    do{perror(msg); exit(EXIT_FAILURE);}while(0);
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        sprintf("%s Usage : ip/port/filename\n", argv[0]);
+    }
+
+    char ip[20] = {0}, filename[1024] = {0}, buff[1024] = {0};
+    int port, sockfd;
+    port = atoi(argv[[2]]);
+    strcpy(ip, argv[1]);
+    strcpy(filename, argv[3]);
+    if ((sockfd = socket_connect))
+
+    return 0;
+}
+
+
